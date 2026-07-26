3db2825dd5cd733ca33bea2a2caafe1bbf1482ac
James Kelly <jamesdkelly88@outlook.com>
2026-07-26 13:07:50 +0100

    tidying up for main

--- .scripts/build-indexes.sh
+++ .scripts/build-indexes.sh
@@ -13,7 +13,7 @@ mapfile -t DIRS < <(find $ROOT -not -path '*/[@.]*' -type d)
 echo "Directories: ${#DIRS[@]}"
 
 F=index.html
-P="/alpine-apks/"
+P="/alpine-apks"
 
 for D in "${DIRS[@]}"; do
     SUBDIR="${D/$ROOT/$P}"
@@ -23,14 +23,14 @@ for D in "${DIRS[@]}"; do
     (
         echo -e "<html>\n  <head>\n    <title>Index of ${SUBDIR}</title>\n  </head>\n  <body>\n    <h1>Index of ${SUBDIR}</h1>\n    <pre>"
         if [ $SUBDIR != $P ]; then
-            echo -e "      <a href="../">../</a>"
+            echo -e "  <a href="../">../</a>"
         fi
         (
             # change IFS locally within subshell so the for loop saves line correctly to L var
             IFS=$'\n';
 
             # pretty sweet, will mimick the normal apache output
-            for L in $(find -L . -mount -depth -maxdepth 1 -type f ! -name 'index.html' -printf "      <a href=\"%f\">%-44f@_@%Td-%Tb-%TY %Tk:%TM  @%f@\n"|sort|sed 's,\([\ ]\+\)@_@,</a>\1,g');
+            for L in $(find -L . -mount -depth -maxdepth 1 -type f ! -name 'index.html' -printf "  <a href=\"%f\">%-44f@_@%Td-%Tb-%TY %Tk:%TM  @%f@\n"|sort|sed 's,\([\ ]\+\)@_@,</a>\1,g');
             do
                 # file
                 F=$(sed -e 's,^.*@\([^@]\+\)@.*$,\1,g'<<<"$L");
@@ -43,7 +43,7 @@ for D in "${DIRS[@]}"; do
             done;
         )
         # now output a list of all directories in this dir (maxdepth 1) other than '.' outputting in a sorted manner exactly like apache
-        find -L . -mount -depth -maxdepth 1 -type d ! -name '.*' -printf "      <a href=\"%f\">%-43f@_@%Td-%Tb-%TY %Tk:%TM  -\n"|sort -d|sed 's,\([\ ]\+\)@_@,/</a>\1,g'
+        find -L . -mount -depth -maxdepth 1 -type d ! -name '.*' -printf "  <a href=\"%f\">%-43f@_@%Td-%Tb-%TY %Tk:%TM  -\n"|sort -d|sed 's,\([\ ]\+\)@_@,/</a>\1,g'
 
         echo -e "    </pre>\n  </body>\n</html"
     ) > $F;
